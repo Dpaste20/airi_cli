01b9c946840dbaf223f5766c5d94ba33d5f2152a
Dhruv <fs20if022@gmail.com>
2025-11-21 18:25:21 +0530

    clean up issue

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from agno.models.ollama import Ollama
 from agno.vectordb.qdrant import Qdrant
 from dotenv import load_dotenv
 
+from utils.asciiArt import ascii_art
+
 logging.getLogger("agno").setLevel(logging.ERROR)
 load_dotenv()
 
@@ -60,34 +62,6 @@ async def run_agent():
 
         except Exception as e:
             print(f"Error: {e}")
-
-
-def print_centered_art(file_path):
-    try:
-        terminal_width = shutil.get_terminal_size().columns
-    except OSError:
-        terminal_width = 80
-
-    with open(file_path, "r") as file:
-        content = file.read()
-
-    lines = content.splitlines()
-
-    max_width = 0
-    if lines:
-        max_width = max(len(line) for line in lines)
-
-    padding = (terminal_width - max_width) // 2
-
-    padding_str = " " * max(0, padding)
-
-    print("\n")
-    for line in lines:
-        print(f"{padding_str}{line}")
-    print("\n")
-
-
-print_centered_art("ascii_art.txt")
 
 
 asyncio.run(run_agent())
